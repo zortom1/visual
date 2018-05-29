a1a6e43198d929469daec0d1d71acd0c2edf46af
zortom1 <zortom@gmail.com>
2018-05-29 20:02:58 +0200

    Add project files.

---
+++ my_repo_project/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace my_repo_project.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public void HelloWorld()
+        {
+
+        }
+    }
+}
---
+++ my_repo_project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="my_repo_project.WebApiApplication" Language="C#" %>
